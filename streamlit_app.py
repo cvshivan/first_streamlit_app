e175901bbf2a64e981d6b41abf9b40dc432f8f18
Shivanandan C V <cvshivan@gmail.com>
2022-05-07 23:01:51 +0530

    Update streamlit_app.py
    ef

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,15 @@ fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list
 #fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),[1,16])
 
-streamlit.dataframe(fruits_selected)
+#streamlit.dataframe(fruits_selected)
 
 
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #fruits_to_show = my_fruit_list
 # print(fruits_to_show)
 
 # display the table on the page
-#streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 
 #good_keys = df.index.intersection(all_keys)
