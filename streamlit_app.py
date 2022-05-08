3b39580c5fc232776828b6478f1ee2e27c1be259
Shivanandan C V <cvshivan@gmail.com>
2022-05-08 20:10:27 +0530

    Update streamlit_app.py
    ef

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include
-#fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.Fruit),['Avocado'])
+fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.Fruit),['Avocado','Strawberries'])
 #fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),index(['Avocado','Strawberries']))
-fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),[1,16])
+#fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),[1,16])
 
 #streamlit.dataframe(fruits_selected)
 
