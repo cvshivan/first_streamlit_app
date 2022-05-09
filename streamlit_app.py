0d4f350ee9dd70d3812a3396eb341384648a1d46
Shivanandan C V <cvshivan@gmail.com>
2022-05-09 11:54:47 +0530

    Update streamlit_app.py
    Errors fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,23 +14,14 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.Fruit),['Avocado','Strawberries'])
-#fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),index(['Avocado','Strawberries']))
-#fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),[1,16])
-
-#streamlit.dataframe(fruits_selected)
+#fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.Fruit),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index),[1,16])     # working based on index
 
 
-#fruits_to_show = my_fruit_list.loc[[1,16]] # working
-#fruits_to_show = my_fruit_list.loc[['Avocado']]
+
+#fruits_to_show = my_fruit_list.loc[[1,16]]     # working based on index
 fruits_to_show = my_fruit_list.loc[my_fruit_list['Fruit'].isin(fruits_selected)]
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-#fruits_to_show = my_fruit_list
-# print(fruits_to_show)
 
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-
-#good_keys = df.index.intersection(all_keys)
-#df.loc[good_keys]
