7752efedf2be19d0707ec0b29a3aaf034ee3f534
Shivanandan C V <cvshivan@gmail.com>
2022-05-30 10:50:26 +0530

    trial

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_fruityvice_data(this_fruit_choice):
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
@@ -84,8 +84,16 @@ streamlit.stop()
 
 
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding',add_my_fruit)
+def insert_row_snowflake(new_fruit):
+  with mycnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    return "Thanks for adding " + new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like to add?)
+if streamlit.button('Add a Fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)                                   
+                                    
+#streamlit.write('Thanks for adding',add_my_fruit)
 
-# This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
